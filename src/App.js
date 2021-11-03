c1bc5b3ea89560f3f9ee657a91ffff44bf7558a4
gabrielvilladiego <89112827+gabrielvilladiego@users.noreply.github.com>
2021-11-03 13:15:43 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./components/login/login";
+import TopMenu from "./components/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <TopMenu />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
